24998c28e8018db17de567e39c307e9482bd8ccb
Matthew Heering <matthew.c.heering01@gmail.com>
2025-06-07 19:24:01 -0500

    added simplecov for determining code coverage on unit tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,17 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  enable_coverage :branch
+  add_filter '/test/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Jobs', 'app/jobs'
+end
+
+puts "Code coverage tracking is on with SimpleCov"
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
