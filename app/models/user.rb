95edd7078904e5e06e4c62f486a8bc2b8fc4d0e9
William Pevytoe <williampevytoe@gmail.com>
2025-06-10 23:05:40 -0400

    Added User confirmation Added Faker and created Seed file to create 100 users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  before_validation :set_full_name
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def set_full_name
+    self.name = "#{first_name} #{last_name}".strip
+  end
 end
