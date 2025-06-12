e6ba486ef12abc9ea8151800e5e533654e852c94
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 06:26:58 -0500

    Created controller for Employees

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "employees/index"
+  get "employees/show"
+  get "employees/new"
+  get "employees/edit"
   resources :rails
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
