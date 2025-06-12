6d9e2c2493354ce31f219083e3d7f5ecc6c40606
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 08:20:45 -0500

    Creating links to navigate between employees, administrators, and users.  Employees is the root.

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,7 +1,7 @@
 class Administrator < ApplicationRecord
   self.table_name = "employees"
 
-  def self.all_supervisors
+  def self.all_administrators
     where(is_administrator: true).select(:id, :first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :employees
+  resources :supervisors, only: [ :index ]
+  resources :administrators, only: [ :index ]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,10 +9,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+   # Defines the root path route ("/")
+   root "employees#index"
 end
