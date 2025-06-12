9d1230db375c4ed5dcb848ea2a15810efcc1e4cd
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 04:14:13 -0500

    Dropped all tables and created employee table using Devise

---
+++ db/migrate/20250611024558_add_fields_to_employees.rb
@@ -0,0 +1,7 @@
+class AddFieldsToEmployees < ActiveRecord::Migration[8.0]
+  def change
+    # add_column :employees, :first_name, :string, null: false
+    # add_column :employees, :last_name, :string, null: false
+    # add_column :employees, :name, :string
+  end
+end
