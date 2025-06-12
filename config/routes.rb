0381052f42efc5ab723693ce5815ddaad9ec2764
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 06:35:17 -0500

    Created code for employee index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get "employees/index"
-  get "employees/show"
-  get "employees/new"
-  get "employees/edit"
-  resources :rails
+  resources :employees
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
