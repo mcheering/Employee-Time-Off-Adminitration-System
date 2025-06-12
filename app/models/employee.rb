b2c6415a9a61e83d8a0af6caaf1930fbfd37a98b
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 04:29:19 -0500

    Created employee model

---
+++ app/models/employee.rb
@@ -0,0 +1,19 @@
+class Employee < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  self.table_name = "employees"
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  before_validation :set_full_name
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def set_full_name
+    self.name = "#{first_name} #{last_name}".strip
+  end
+end
