a2e900f2b1efc3527887ac0a61187cf3d2ff97fc
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 07:43:03 -0500

    Fixed password issue for new employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,15 +1,62 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: %i[ show edit update]
+
+  # GET /employees or /employees.json
   def index
     @employees = Employee.all
   end
 
+  # GET /employees/1 or /employees/1.json
   def show
   end
 
+  # GET /employees/new
   def new
     @employee = Employee.new
   end
 
+  # GET /employees/1/edit
   def edit
   end
+
+  # POST /employees or /employees.json
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.password_confirmation = @employee.password
+
+    respond_to do |format|
+      if @employee.save
+        format.html { redirect_to @employee, notice: "Employee was successfully created." }
+        format.json { render :show, status: :created, location: @employee }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /employees/1 or /employees/1.json
+  def update
+    respond_to do |format|
+      if @employee.update(employee_params)
+        format.html { redirect_to @employee, notice: "Employee was successfully updated." }
+        format.json { render :show, status: :ok, location: @employee }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_employee
+      @employee = Employee.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def employee_params
+      params.expect(employee: [ :first_name, :last_name, :email, :password, :hire_date, :termination_date, :is_administrator, :is_supervisor, :supervisor_id ])
+    end
 end
