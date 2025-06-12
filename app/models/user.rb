739f0c5bdd08b212e7a40e6925a8427fbcdfdf3c
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 04:29:47 -0500

    Modified user model to use employee table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  self.table_name = "employees"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   before_validation :set_full_name
 
   validates :first_name, :last_name, :email, presence: true
