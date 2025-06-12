e250105f9ace8f1b62c036df703e4aa000c8d4ba
Terry Thompson <tt00193@my.westga.edu>
2025-06-12 06:08:49 -0500

    Added fields to employee model

--- db/migrate/20250612104618_add_employee_fields.rb
+++ db/migrate/20250612104618_add_employee_fields.rb
@@ -1,4 +1,11 @@
 class AddEmployeeFields < ActiveRecord::Migration[8.0]
   def change
+    add_reference :employees, :supervisor, foreign_key: { to_table: :employees }, index: true
+    add_column :employees, :first_name, :string, null: false
+    add_column :employees, :last_name, :string, null: false
+    add_column :employees, :hire_date, :date, null: false
+    add_column :employees, :termination_date, :date
+    add_column :employees, :is_supervisor, :boolean, default: false
+    add_column :employees, :is_administrator, :boolean, default: false
   end
 end
