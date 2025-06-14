8c7e23049763b1ce6f0ff9f6a5ddad4d03aa94ae
Terry Thompson <tt00193@my.westga.edu>
2025-06-13 20:09:32 -0400

    Created attributes for fiscal year

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -1,4 +1,14 @@
+# Authors: William Pevytoe, Terry Thompson
+# Date: 2024-06-20
+# Description: Model of a company's accounting year.
 class FiscalYear < ApplicationRecord
     has_many :fiscal_year_employees
     has_many :employees, through: :fiscal_year_employees
+
+    # Author: Terry Thompson
+    # Date: 2024-06-20
+    # Description: Displays a string that indicates the fiscal year
+    def caption
+        start_date.year == end_date.year ? "#{start_date.year}" : "#{start_date.year}-#{end_date.strftime('%y')}"
+    end
 end
