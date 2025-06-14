72e788a775fa339dc387bbf694bdc75781a7cc8a
Terry Thompson <tt00193@my.westga.edu>
2025-06-14 19:21:55 -0400

    Added documentation to the employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
+# Authors: William Pevytoe, Terry Thompson
+# Date: 2024-06-20
+# Description: Model of a person who works for the company.
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -12,10 +15,16 @@ class Employee < ApplicationRecord
   has_many :fiscal_year_employees
   has_many :fiscal_years, through: :fiscal_year_employees
 
+  # Author: Terry Thompson
+  # Date: 2024-06-20
+  # Description: Displays the employee's full name
   def name
     "#{first_name} #{last_name}"
   end
 
+  # Author: Terry Thompson
+  # Date: 2024-06-20
+  # Description: Displays the supervisor's full name
   def supervisor_name
     supervisor = Employee.find_by(id: supervisor_id)
     supervisor ? supervisor.name : "none"
