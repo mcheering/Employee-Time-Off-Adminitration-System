03a69890e502db68b5881e77dda1863708c966ee
Matthew Heering <matthew.c.heering01@gmail.com>
2025-06-16 11:51:57 -0500

    updated views to be more organized, editing functinality complete and tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'employees/new', to: 'employees#new', as: :new_employee
+  get    'employees',          to: 'employees#index',  as: :employees
+  post   'employees',          to: 'employees#create'
+  get    'employees/new',      to: 'employees#new',    as: :new_employee
+  get    'employees/:id/edit', to: 'employees#edit',   as: :edit_employee
+  get    'employees/:id',      to: 'employees#show',   as: :employee
+  patch  'employees/:id',      to: 'employees#update'
+  put    'employees/:id',      to: 'employees#update'
+  delete 'employees/:id',      to: 'employees#destroy'
 
-  resources :employees, except: [:new]
   resources :supervisors, only: [:index]
   resources :administrators, only: [:index]
   devise_for :users
@@ -12,12 +18,8 @@ Rails.application.routes.draw do
     end
   end
 
-  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # gets the admin dashboard
   get "/admin/dashboard", to: "administrators#dashboard", as: "admin_dashboard"
-  
-  # Root path
+
   root "employees#index"
 end
