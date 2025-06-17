908f82936a5c1af2f8de456eb10cae1b90c8eaaa
Terry Thompson <tt00193@my.westga.edu>
2025-06-16 20:43:20 -0500

    Tested is_open defaults to true and that caption displays correctly

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -2,10 +2,11 @@
 # Date: 2024-06-20
 # Description: Model of a company's accounting year.
 class FiscalYear < ApplicationRecord
+    after_initialize :set_default_is_open
     has_many :fiscal_year_employees
     has_many :employees, through: :fiscal_year_employees
 
-    validates :start_date, :end_date, presence: true
+    validates :start_date, :end_date, :is_open, presence: true
     validate :start_date_before_end_date
 
     # Author: Terry Thompson
@@ -15,11 +16,17 @@ class FiscalYear < ApplicationRecord
         start_date.year == end_date.year ? "#{start_date.year}" : "#{start_date.year}-#{end_date.strftime('%y')}"
     end
 
+    # Author: Terry Thompson
+    # Date: 2024-06-20
+    # Description: Displays a string that whether the fiscal year is open or closed
     def status
       is_posted ? "open" : "closed"
     end
 
     private
+    def set_default_is_open
+        self.is_open ||= true
+    end
     def start_date_before_end_date
         if start_date.present? && end_date.present? && start_date >= end_date
             errors.add(:start_date, "must be before end date")
--- test/models/fiscal_year_test.rb
+++ test/models/fiscal_year_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class FiscalYearTest < ActiveSupport::TestCase
-  # Test that a fiscal year can only be created with valid attributes
-  # This test checks that the fiscal year model requires start_date and end_date to be present
-  # and that it is invalid if these attributes are not provided.
+  # Author: Terry Thompson
+  # Date: 6/16/2025
+  # Description: Tests that a fiscal year can only be created with valid attributes.
   def test_should_create_fiscal_year_with_valid_attributes
     fiscal_year = FiscalYear.new
     assert fiscal_year.invalid?
@@ -12,10 +12,36 @@ class FiscalYearTest < ActiveSupport::TestCase
     assert fiscal_year.errors[:is_open].none?
   end
 
-  # Test that the start_date must be before the end_date
+  # Author: Terry Thompson
+  # Date: 6/16/2025
+  # Tests that the start_date must be before the end_date
   def test_start_date_before_end_date
     fiscal_year = FiscalYear.new(start_date: Date.today, end_date: Date.yesterday)
     assert fiscal_year.invalid?
     assert_includes fiscal_year.errors[:start_date], "must be before end date"
+  end
+
+  # Author: Terry Thompson
+  # Date: 6/16/2025
+  # Tests that is_open defaults to true for new fiscal years
+  def test_fiscal_year_is_open_default_is_true
+    fiscal_year = FiscalYear.new(start_date: Date.today, end_date: Date.tomorrow)
+    assert fiscal_year.is_open?
+  end
+
+  # Author: Terry Thompson
+  # Date: 6/16/2025
+  # Tests that caption returns a single year when the company uses a calendar year for its fiscal year
+  def test_caption_is_correct_for_same_year
+    fiscal_year = FiscalYear.new(start_date: Date.new(2024, 1, 1), end_date: Date.new(2024, 12, 31))
+    assert_equal "2024", fiscal_year.caption
+  end
+
+  # Author: Terry Thompson
+  # Date: 6/16/2025
+  # Tests that caption returns a string in the format "YYYY-YY" when the fiscal year spans two years
+  def test_caption_is_correct_for_different_years
+    fiscal_year = FiscalYear.new(start_date: Date.new(2024, 7, 1), end_date: Date.new(2025, 06, 30))
+    assert_equal "2024-25", fiscal_year.caption
   end
 end
