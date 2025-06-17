8c6473cf2e8d646c9ecc053ec47a38f4dbb2ac5e
Terry Thompson <tt00193@my.westga.edu>
2025-06-17 14:38:42 -0500

    Completed Company tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@
 # Date: 2024-06-20
 # Description: Model of a company.
 class Company < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create company with valid attributes" do
+    company = Company.new
+    assert company.invalid?
+    assert company.errors[:name].any?
+  end
 end
