59e94dd139b84384fb94b19351ed80f16c626d27
Terry Thompson <tt00193@my.westga.edu>
2025-06-16 20:14:30 -0500

    Tested that start_date must be before end date

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -5,10 +5,20 @@ class FiscalYear < ApplicationRecord
     has_many :fiscal_year_employees
     has_many :employees, through: :fiscal_year_employees
 
+    validates :start_date, :end_date, presence: true
+    validate :start_date_before_end_date
+
     # Author: Terry Thompson
     # Date: 2024-06-20
     # Description: Displays a string that indicates the fiscal year
     def caption
         start_date.year == end_date.year ? "#{start_date.year}" : "#{start_date.year}-#{end_date.strftime('%y')}"
+    end
+
+    private
+    def start_date_before_end_date
+        if start_date.present? && end_date.present? && start_date >= end_date
+            errors.add(:start_date, "must be before end date")
+        end
     end
 end
