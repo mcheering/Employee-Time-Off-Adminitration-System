0083fd4c8aec29f4af02d8f85bbdc1cff14ee214
Terry Thompson <tt00193@my.westga.edu>
2025-06-16 19:40:52 -0500

    created test files for employee and fiscal year models.

---
+++ test/models/employee_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class EmployeeTest < ActiveSupport::TestCase
+  # Test that an employee can only ßbe created with valid attributes
+  # test "should create employee with valid attributes" do
+  #  employee = Employee.new
+  #   assert employee.invalid?
+  #   assert employee.errors[:first_name].any?
+  #   assert employee.errors[:last_name].any?
+  #   assert employee.errors[:hire_date].any?
+  #   assert employee.errors[:is_administrator].any?
+  #   assert employee.errors[:is_supervisor].any?
+  #   assert employee.errors[:email].any?
+  #   assert employee.errors[:password].any?
+  # end
+end
--- test/models/fiscal_year_test.rb
+++ test/models/fiscal_year_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class FiscalYearTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test that a fiscal year can only be created with valid attributes
+  # This test checks that the fiscal year model requires start_date and end_date to be present
+  # and that it is invalid if these attributes are not provided.
+  def test_should_create_fiscal_year_with_valid_attributes
+    fiscal_year = FiscalYear.new
+    assert fiscal_year.invalid?
+    assert fiscal_year.errors[:start_date].any?
+    assert fiscal_year.errors[:end_date].any?
+  end
 end
