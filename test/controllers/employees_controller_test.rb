cebac22c993f18e8944ef7cf03e6db3b40bb596a
wpevyto1 <wpevyto1@my.westga.edu>
2025-06-20 22:35:35 -0400

    Finished employee controller test to get full coverage

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -57,6 +57,28 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
   end
 
   # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should error on creating employee" do
+    initial_count = Employee.count
+
+    post employees_url, params: {
+      employee: {
+        email: "",
+        password: "password123",
+        password_confirmation: "password123",
+        first_name: "",
+        last_name: "",
+        hire_date: "",
+        is_supervisor: false,
+        is_administrator: false
+      }
+    }
+    assert_equal initial_count, Employee.count
+    assert_response :unprocessable_entity
+    assert_select "#new-employee-form"
+  end
+
+  # Author: William Pevytoe
   # Date: 6/17/2025
   test "should show employee" do
     get employee_url(@employee)
@@ -83,12 +105,76 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
   end
 
   # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should update employee password when provided" do
+    refute @employee.valid_password?("NewSecret123!")
+    patch employee_url(@employee), params: {
+      employee: {
+        password:              "NewSecret123!",
+        password_confirmation: "NewSecret123!"
+      }
+    }
+    assert_redirected_to employee_path(@employee)
+    @employee.reload
+    assert @employee.valid_password?("NewSecret123!")
+  end
+
+  # Author: William Pevytoe
   # Date: 6/17/2025
   test "should destroy employee" do
     assert_difference("Employee.count", -1) do
       delete employee_url(@employee)
     end
-
     assert_redirected_to employees_url
+  end
+
+  # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should not destroy non-existent employee" do
+    missing_id = @employee.id + 9999
+    assert_no_difference("Employee.count") do
+      delete employee_url(missing_id)
+    end
+    assert_redirected_to employees_url
+    assert_equal "Employee not found.", flash[:alert]
+  end
+
+  # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should error on updating employee with invalid data" do
+    original_last = @employee.last_name
+    patch employee_url(@employee), params: {
+      employee: { last_name: "" }
+    }
+    assert_response :unprocessable_entity
+    @employee.reload
+    assert_equal original_last, @employee.last_name
+  end
+  
+  # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should redirect show when employee not found" do
+    missing_id = Employee.last.id + 1_000
+    get employee_url(missing_id)
+    assert_redirected_to employees_url
+    assert_equal "Employee not found.", flash[:alert]
+  end
+
+  # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should redirect edit when employee not found" do
+    missing_id = Employee.last.id + 1_000
+    get edit_employee_url(missing_id)
+    assert_redirected_to employees_url
+    assert_equal "Employee not found.", flash[:alert]
+  end
+
+  # Author: William Pevytoe
+  # Date: 6/20/2025
+  test "should redirect update when employee not found" do
+    missing_id = Employee.last.id + 1_000
+    patch employee_url(missing_id), params: { employee: { last_name: "Nope" } }
+    assert_redirected_to employees_url
+    assert_equal "Employee not found.", flash[:alert]
   end
 end
