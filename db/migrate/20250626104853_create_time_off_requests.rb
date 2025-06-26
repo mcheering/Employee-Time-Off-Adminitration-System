92d01399e5870f58dfbc4e8485fcf253536837be
Terry Thompson <tt00193@my.westga.edu>
2025-06-26 06:32:22 -0500

    Added not null to table attributes.

--- db/migrate/20250626104853_create_time_off_requests.rb
+++ db/migrate/20250626104853_create_time_off_requests.rb
@@ -1,10 +1,9 @@
 class CreateTimeOffRequests < ActiveRecord::Migration[8.0]
   def change
     create_table :time_off_requests do |t|
-      t.integer :employee_id
-      t.integer :supervisor_id
-      t.integer :submitted_by
-      t.integer :fiscal_year_id
+      t.integer :fiscal_year_employee_id, null: false
+      t.integer :supervisor_id, null: false
+      t.integer :submitted_by, null: false
       t.date :request_date
       t.integer :reason
       t.boolean :is_fmla
