9196f97409ae8bc262b57e5066e9a2eb98fb3f0e
Terry Thompson <tt00193@my.westga.edu>
2025-06-26 06:32:22 -0500

    Added not null to table attributes.

--- db/migrate/20250626105205_create_time_offs.rb
+++ db/migrate/20250626105205_create_time_offs.rb
@@ -1,10 +1,11 @@
 class CreateTimeOffs < ActiveRecord::Migration[8.0]
   def change
     create_table :time_offs do |t|
-      t.integer :request_id
-      t.float :amount
-      t.boolean :was_taken
-      t.integer :decision
+      t.integer :request_id, null: false
+      t.date    :date,       null: false
+      t.float   :amount,     null: false
+      t.boolean :was_taken,               default: true
+      t.integer :decision,   null: false, default: 0
 
       t.timestamps
     end
