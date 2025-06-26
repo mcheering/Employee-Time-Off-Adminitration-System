0749432e01c66c74e2704f5dc18468410af83348
Terry Thompson <tt00193@my.westga.edu>
2025-06-26 08:07:39 -0500

    Updated captions

--- app/models/time_off.rb
+++ app/models/time_off.rb
@@ -3,11 +3,18 @@ class TimeOff < ApplicationRecord
 
   validates :date, presence: true
   validates :time_off_request_id, presence: true
+  validates :was_taken, inclusion: { in: [ true, false ] }
 
   delegate :fiscal_year_employee_name, to: :time_off_request, prefix: true
   delegate :supervisor_name, to: :time_off_request, prefix: true
 
+  enum time_off_decision: { none: 0, approved: 1, denied: 2 }
+
   def self.for_request(request_id)
     where(time_off_request_id: request_id)
+  end
+
+  def decision_caption
+    I18n.t("time_off.decision.#{time_off_decision}")
   end
 end
