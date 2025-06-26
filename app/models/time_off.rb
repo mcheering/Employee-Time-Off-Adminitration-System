411ca7ab5c6c0f0abc67d8fc79034409bd6bc6f8
Terry Thompson <tt00193@my.westga.edu>
2025-06-26 07:05:00 -0500

    Updated time_off_request model to include additional_information_date

--- app/models/time_off.rb
+++ app/models/time_off.rb
@@ -1,2 +1,13 @@
 class TimeOff < ApplicationRecord
+  belongs_to :time_off_request, class_name: "TimeOffRequest", foreign_key: "time_off_request_id"
+
+  validates :date, presence: true
+  validates :time_off_request_id, presence: true
+
+  delegate :fiscal_year_employee_name, to: :time_off_request, prefix: true
+  delegate :supervisor_name, to: :time_off_request, prefix: true
+
+  def self.for_request(request_id)
+    where(time_off_request_id: request_id)
+  end
 end
