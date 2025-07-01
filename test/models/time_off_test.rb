4df7e93da4fc68c1e1229ca59d3ea8a2d9307727
Terry Thompson <tt00193@my.westga.edu>
2025-06-30 22:14:38 -0500

    Tested remaining decision caption values.

--- test/models/time_off_test.rb
+++ test/models/time_off_test.rb
@@ -33,4 +33,15 @@ class TimeOffTest < ActiveSupport::TestCase
 
   # Author: Terry Thompson
   # Date: 6/30/2025
+  test "decision_caption equals approved when decision is approved" do
+    time_off = TimeOff.new(decision: "approved")
+    assert_equal "approved", time_off.decision_caption
+  end
+
+  # Author: Terry Thompson
+  # Date: 6/30/2025
+  test "decision_caption equals denied when decision is denied" do
+    time_off = TimeOff.new(decision: "denied")
+    assert_equal "denied", time_off.decision_caption
+  end
 end
